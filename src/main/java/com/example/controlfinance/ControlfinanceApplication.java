2e0827b8ea9210cacdcc2657b4cbe9587f2a164d
unknown <luisfernandonunesprofissional@gmail.com>
2024-09-30 12:50:10 -0300

    project control finance created

---
+++ src/main/java/com/example/controlfinance/ControlfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlfinanceApplication.class, args);
+	}
+
+}
